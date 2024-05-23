a0dae333093ba285d8141c9425ec6bbcb5d8be1a
armen1s <79501431+armen1s@users.noreply.github.com>
2024-05-23 19:50:21 +0300

    sl.py

--- sl.py
+++ sl.py
@@ -1,25 +1,33 @@
-# sl.py
-
 import pandas as pd
 
 class SL:
-    def init(self, is_dynamic: bool=False, coef: float=0.25):
+    def init(self, is_dynamic: bool = False, coef: float = 0.25):
+        """
+        Инициализация объекта SL (Stop Loss).
+
+        Args:
+        is_dynamic (bool): Определяет, будет ли стоп-лосс динамическим.
+        coef (float): Коэффициент для расчёта стоп-лосса от текущей цены.
+        """
         self.is_dynamic = is_dynamic
         self.coef = coef
 
-    def get_dynamic_sl(self, df: pd.DataFrame, position_type: str) -> float:
-        price = df['close'].iloc[-1]
-        if position_type == 'buy':
-            return price - price * self.coef
-        elif position_type == 'sell':
-            return price + price * self.coef
+    async def calculate_sl(self, df: pd.DataFrame, position_type: str) -> float:
+        """
+        Асинхронно рассчитывает стоп-лосс на основе последней закрытой цены и заданного коэффициента.
 
-    def get_sl(self, df: pd.DataFrame, position_type: str) -> float:
+        Args:
+        df (pd.DataFrame): DataFrame, содержащий данные о ценах.
+        position_type (str): Тип позиции ('buy' или 'sell').
+
+        Returns:
+        float: Рассчитанный уровень стоп-лосса.
+        """
         price = df['close'].iloc[-1]
-        if not self.is_dynamic:
-            if position_type == 'buy':
-                return price - price * self.coef
-            elif position_type == 'sell':
-                return price + price * self.coef
+        adjustment = price * self.coef
+        if position_type == 'buy':
+            return price - adjustment
+        elif position_type == 'sell':
+            return price + adjustment
         else:
-            return self.get_dynamic_sl(df, position_type)
+            raise ValueError("Invalid position type provided. Use 'buy' or 'sell'.")
